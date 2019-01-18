8474990768d60daff08e5cd8f3b897992fe067da
Austin Joo <joojinhyung@gmail.com>
2019-01-18 12:11:12 -0500

    Edits made to artillery config

--- artilleryFunctions.js
+++ artilleryFunctions.js
@@ -7,7 +7,7 @@ module.exports = {
 var randomizer = (context, events, done) => {
     let id = parseInt(Math.random()*10000000);
     context.vars.id = id;
-    return done();
+    return done(id);
 }
 
 
